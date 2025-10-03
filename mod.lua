5ff5f3dbca8c743c9741e11752e51d9925fb5002
Paul <paul.turkovskiy@gmail.com>
2025-10-03 15:24:42 +0300

    fix

--- mod.lua
+++ mod.lua
@@ -1,4 +1,15 @@
 Hooks:PostHook(ConnectionNetworkHandler, "set_dropin", "set_dropin_CFA", function()
   Global.statistics_manager.playing_from_start = true
-  log("[CrashFriendlyAchievements] playing_from_start = true")
+  log("[CrashFriendlyAchievements] Overriding drop-in flag on network request")
+end)
+
+Hooks:PostHook(StatisticsManager, "start_session", "start_session_CFA", function(self, data)
+    Global.statistics_manager.playing_from_start = true
+    self._start_session_drop_in = false
+    self._start_session_from_beginning = true
+    log("[CrashFriendlyAchievements] Overriding drop-in flag on session start")
+end)
+
+Hooks:OverrideFunction(StatisticsManager, "is_dropin", function(self)
+    return false
 end)
